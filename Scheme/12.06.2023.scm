ca8048824435239c90d068777413a237706eca54
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-13 11:27:07 +0200

    Update 12.06.2023.scm

--- Scheme/12.06.2023.scm
+++ Scheme/12.06.2023.scm
@@ -5,3 +5,9 @@
 ;important: the implementation must be one-pass, for efficiency reasons, i.e. it must consider each element of the
 ;input list only once; hence it is not correct to just call Scheme’s fold-left and -right.
 ;Example: (fold-left-right string-append "" '("a" "b" "c")) is the pair ("cba" . "abc").
+
+
+;we need to create a cons node where the car contains the fold-left and the cdr contains the fold-right
+
+(define (fold-left-right  f L) )
+
