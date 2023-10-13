a799afe3387bc2f0b2d3364a04e742d35dd77603
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-13 11:45:51 +0200

    Update 12.06.2023.scm

--- Scheme/12.06.2023.scm
+++ Scheme/12.06.2023.scm
@@ -7,7 +7,23 @@
 ;Example: (fold-left-right string-append "" '("a" "b" "c")) is the pair ("cba" . "abc").
 
 
-;we need to create a cons node where the car contains the fold-left and the cdr contains the fold-right
+(define (fold-left-right f i l)
+  ; Define the fold-left-right function that takes three arguments:
+  ; f - a binary function to be applied to the elements
+  ; i - the initial value for folding
+  ; l - the list to be folded
+  
+  (let loop ((left i)          ; Define a nested function 'loop' with parameters:
+             (right (lambda (x) x)) ; Initialize 'right' as an identity function
+             (xs l))          ; Initialize 'xs' with the input list 'l'
+    ; This is a tail-recursive loop that will iterate through the list 'l'.
+  
+    (if (null? xs)            ; If we have reached the end of the list:
+        (cons left (right i)) ; Return a cons node where 'left' is the result of fold-left
+                              ; and 'right' is the result of fold-right.
+      (loop (f (car xs) left) ; Apply the binary function 'f' to the head of the list and 'left'
+            (lambda (x)       ; Define a lambda function that takes 'x' as a parameter:
+              (right (f (car xs) x))) ; Apply 'f' to the head of the list and the result 'x' for 'right'
+            (cdr xs)))))     ; Move to the next element of the list by taking the cdr.
 
-(define (fold-left-right  f L) )
 
