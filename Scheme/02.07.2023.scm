7e8ec0e8dcaaf90501cdc8beaea7788320d132da
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-13 11:07:30 +0200

    Update 02.07.2023.scm

--- Scheme/02.07.2023.scm
+++ Scheme/02.07.2023.scm
@@ -8,3 +8,26 @@
 ; (a (b 1) (c (+ b 1)) d (e (+ d 1)) f)
 ; (list a b c d e f))
 ;should return '(#f 1 2 2 3 3), because a assumes the default value #f, while d = c and f = e.
+
+
+;I would use a macro to define let** construct
+(define-syntax let** 
+  (define syntax-rules (def:) ;we do need to pass an additional keyword
+
+    (
+
+    ;start with the base cases
+     ((_ def: v (var) istr... ) ;v is the input default value 
+      (lambda (var) ( istr ...) v)
+
+     ((_ def: v ((var val)) istr... )
+      (lambda (var) (istr ...) val)
+
+    ;general cases
+     (_ def: v ((var val) . rest) istr... ) ;note that the rest is not necessarily a (var val) but something else, i.e. it is a list of something
+      (lambda (var) (let** def: val rest istr ...) val) ; to know whic was the last defined we change the default value
+
+     (_ def: v ((var) . rest) istr... )
+      (lambda (var) (let** def: v rest istr ...) v)   
+
+    ) )
