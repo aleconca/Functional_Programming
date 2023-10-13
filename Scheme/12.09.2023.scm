0be8dab9a6b80d810be08f25de2a850eff60ad96
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-13 10:04:13 +0200

    Update 12.09.2023.scm

--- Scheme/12.09.2023.scm
+++ Scheme/12.09.2023.scm
@@ -14,18 +14,19 @@
 ;my solution involves macros since we want to define a new keyword, something
 ;similar to what we did in class with the while construct.
 
-;Hypothesis: we do have more than 1 function and more than 1 body 
+;Hypothesis: we do have more than 1 function and more than 1 body and
 ;the goal is just defining functions so we do not need to care about ret values 
 ;in the begin block[*]
 
 (define-syntax multifun
-  (syntax-rules () ;nothing more to pass to the constrct
+  (syntax-rules () ;no additional syntax to pass to the constrct
   
     ;we need to match a list of fs, list of params and bodies
     ;so we use (f . rest1) which is a way to define a list with a cons 
-    ;node but which highlights the first element (f) and the remaining ones (rest1)
-    ;the ... states that we can have more bodies and _ matches multifun keyword
-    ;as parameters are the same for each one I write (x ...) which means 
+    ;node but which highlights the first element (f, the car) and the remaining ones (rest1, the cdr);
+    ;we do this for functions and bodies.
+    ; _ matches multifun keyword.
+    ;as parameters are the same list for each one I write (x ...) which means 
     ;'one or more parameter'
     ((_ (f . rest1) (x ...) (b . rest3) )
     
@@ -33,10 +34,10 @@
       (begin 
       
       ;now we need to define a way to apply functions and bodies
-      ;f is alraedy defined so we only need to apply it to the params and bodies
+      ;f is alraedy defined so we only need to apply it to the params and match it with its body
       (define (f (x ...)) 
       
-              ;define the respective bodies
+              ;define the respective body
               b)  
       
       ;since we have more than one f we make the macro recursive->tail-recursive call
@@ -59,4 +60,15 @@
 ;expression1, expression2, ..., expressionN: 
 ;These are the expressions that you want to evaluate sequentially. 
 ;Each expression can be any valid Scheme expression.
+;begin can return 1 value.
+
+
+
+
+
+;Would be possible to define something similar, but using a procedure and lambda functions instead of a
+;macro? If yes, do it; if no, explain why.
+
+;SOL:
+
 
