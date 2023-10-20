ab138b0221429fb3a81a9b49164cf64f738b916a
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-20 09:56:34 +0200

    Create 14.07.2021-macro.scm

---
+++ Scheme/14.07.2021-macro.scm
@@ -0,0 +1,5 @@
+#lang racket
+
+;Define a defun construct like in Common Lisp, where (defun f (x1 x2 ...) body) is used for defining a
+;function f with parameters x1 x2 ....
+;Every function defined in this way should also be able to return a value x by calling (ret x).
