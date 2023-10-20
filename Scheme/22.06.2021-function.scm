119ba72f633ad59554cf92090a0c427561af3179
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-20 11:10:57 +0200

    Update 22.06.2021-function.scm

--- Scheme/22.06.2021-function.scm
+++ Scheme/22.06.2021-function.scm
@@ -5,3 +5,21 @@
 ;E.g.
 ;(mix (lambda (x) (* x x)) 1 2 3 4 5)
 ;returns: '(1 (2 (3 (4 (5 (1 4 9 16 25) 5) 4) 3) 2) 1)
+
+;define a function that takes as input a function and a list
+(define (mix f L)
+
+  (foldr ((lambda(x y)(x y x)) (map f L) L))
+  
+  )
+
+;difficulty: how can I create that structure?
+;compute foldr and foldl and store them
+;(a*(b*(c)))
+;(c*(b*(a*)))
+;then concatenate this two separated by a middle list (apply f L
+;error-> I would get an incorrect concatenation
+;it is easier to apply a lambda to each element of L, starting form (map f L) while folding right.
+
+
+
