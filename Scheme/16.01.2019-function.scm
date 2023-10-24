102cefc6c9839979e24ee5dff8904a748c5866bd
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-24 09:28:43 +0200

    Delete Scheme/16.01.2019-function.scm

--- Scheme/16.01.2019-function.scm
+++
@@ -1,4 +0,0 @@
-#lang racket
-
-;Define a pure function f with a variable number of arguments, that, when called like (f x1 x2 .. xn), returns:
-;(xn (xn-1 ( .. (x1 (xn xn-1 .. x1))..). Function f must be defined using only fold operations for loops.
