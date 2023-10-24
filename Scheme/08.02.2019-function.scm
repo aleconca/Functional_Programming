89422508f0987be8e6637f8b7a7a59abf37fe80a
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-24 11:07:25 +0200

    Update 08.02.2019-function.scm

--- Scheme/08.02.2019-function.scm
+++ Scheme/08.02.2019-function.scm
@@ -5,3 +5,24 @@
 ;E.g. when called like:
 ;(multi-merge '(1 2 3 4 8) '(-1 5 6 7) '(0 3 8) '(9 10 12))
 ;it returns: '(-1 0 1 2 3 3 4 5 6 7 8 8 9 10 12)
+
+
+;define an helper function to order the lists while merging them
+(define (merge a1 a2)
+   (cond ((null? a1) a2)
+         ((null? a2) a1)
+         (else (let ((x (car a1))
+                     (y (car a2)))
+               (if (< x y)
+                   (cons x (merge (cdr a1) a2))
+                   (cons y (merge a1 (cdr a2))))))))
+
+
+(define (multi-merge . data)
+  ;apply merge to data and store result of foldl in a new list
+  ;why foldl? Tried in Dr Racket with foldr->same result; but still: why does it work with foldl?
+  ;how is it possible that merge takes two inputs but I pass an arbitrary number of them and it still works?
+  (foldl merge '() data))
+
+
+(multi-merge '(1 2 3 4 8) '(-1 5 6 7) '(0 3 8) '(9 10 12))
