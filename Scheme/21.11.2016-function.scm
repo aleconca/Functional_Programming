ece455cf7d2579dbc82b492d29e075f4d74abe1e
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-24 15:59:30 +0200

    Create 21.11.2016-function.scm

---
+++ Scheme/21.11.2016-function.scm
@@ -0,0 +1,5 @@
+#lang racket
+
+;The fold operations are very general, and can be used to implement many higher order functions.
+;1) Define map as a fold (left or right, your choice).
+;2) Define filter as a fold (left or right, your choice).
