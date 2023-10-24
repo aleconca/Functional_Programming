8f82f6206519506f46a43c39e7312676f546f071
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-24 15:56:38 +0200

    Update 16.01.2018-function.scm

--- Scheme/16.01.2018-function.scm
+++ Scheme/16.01.2018-function.scm
@@ -10,3 +10,37 @@
 ;the correct pair of separators are discarded.
 
 ;E.g., in the case of L, the result should be (6 1 3 “hellomyworld”)
+
+
+;SOL
+
+(define (loc-p ls res cint cstr)
+  
+   (if (null? ls)
+        res
+       (let ((cur (car ls)))
+         (cond
+          ((eq? cur '*)
+              (if cint
+                 (loc-p (cdr ls) (append res (list cint)) #f cstr)
+                 (loc-p (cdr ls) res 0 cstr)))
+          ((eq? cur '$)
+              (if cstr
+                 (loc-p (cdr ls) (append res (list cstr)) cint #f)
+                 (loc-p (cdr ls) res cint "")))
+          
+          ((and cint (number? cur))
+                 (loc-p (cdr ls) res (+ cur cint) cstr))
+          ((or (and cstr (string? cur)) (eq? cstr "") (and cstr (not (number? cur))))
+           (loc-p (cdr ls) res cint (append (list cstr) (list cur))))
+          (else
+                 (loc-p (cdr ls) res cint cstr))
+          )
+       )
+       )
+  )
+
+(define (p lst)
+ (loc-p lst '() #f #f))
+
+(p '(* 1 2 3 * $ “hello” * 1 * 7 “my” * 1 2 * “world” $))
