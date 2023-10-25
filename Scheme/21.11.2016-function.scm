246eac872d8ef6752818659f58daa675e8fa121e
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-25 11:22:00 +0200

    Update 21.11.2016-function.scm

--- Scheme/21.11.2016-function.scm
+++ Scheme/21.11.2016-function.scm
@@ -1,5 +1,25 @@
 #lang racket
 
 ;The fold operations are very general, and can be used to implement many higher order functions.
+
+
 ;1) Define map as a fold (left or right, your choice).
+
+(define (fold-map bf dest L)
+  (if (null? L)
+      dest
+      (fold-map bf (cons (bf (car L)) dest) (cdr L))))
+
+ 
+(fold-map (lambda(x)(+ x 1)) '() '(1 2 3))
+
+
 ;2) Define filter as a fold (left or right, your choice).
+
+(define (fold-filter bf dest L)
+  (if (null? L)
+      dest
+      (fold-filter bf (set! dest (bf (car L))) (cdr L))))
+
+ 
+(fold-map (lambda(x)(< x 1)) '() '(1 -2 3))
