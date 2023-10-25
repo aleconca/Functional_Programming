e533e8c4b339955fb76f3930206d3fefc9aa8408
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-25 11:35:08 +0200

    Update 21.11.2016-function.scm

--- Scheme/21.11.2016-function.scm
+++ Scheme/21.11.2016-function.scm
@@ -24,3 +24,23 @@
  
 (fold-map (lambda(x)(< x 1)) '() '(1 -2 3))
 ;'(#f #t #f)
+
+;ERROR: 
+;-remember map and filter take as input two arguments
+;-should have used a foldr in both cases directly
+
+;SOL: easier than expected
+
+(define (fmap f l)
+  ;y is initialized as 0, then used as an accumulator
+  ;ex. iteration 1: x=3, y=0 -> cons=(4)
+  (foldr (lambda (x y) (cons (f x) y)) '() l))
+
+(fmap (lambda(x)(+ x 1)) '(1 2 3))
+;'(2 3 4)
+
+(define (ffilter p l)
+  (foldr (lambda (x y)(if (p x)(cons x y) y)) '() l))
+
+
+
