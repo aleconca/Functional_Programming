06c80007cd27dbe163744ba3f5bc437b1df6864a
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-26 17:46:56 +0200

    Update 08.02.2019-function.scm

--- Scheme/08.02.2019-function.scm
+++ Scheme/08.02.2019-function.scm
@@ -8,6 +8,13 @@
 
 
 ;define an helper function to order the lists while merging them
+;1. a1 => (1 2 3 4 8)
+   ;a2 => ()
+   ;return a1->accumulatore a2=(1 2 3 4 8)
+   
+;2. a1 => (-1 5 6 7)
+   ;a2 => (1 2 3 4 8)
+   
 (define (merge a1 a2)
    (cond ((null? a1) a2)
          ((null? a2) a1)
@@ -20,7 +27,14 @@
 
 (define (multi-merge . data)
   ;apply merge to data and store result of foldl in a new list
-  
+  ;foldl:
+  ;La funzione binaria (merge in questo caso) verrà applicata in modo iterativo agli elementi della lista. 
+  ;Questa funzione deve prendere due argomenti: l'accumulatore (a2='()) e l'elemento corrente della lista (a1), eseguire qualche operazione su di essi e restituire il risultato. 
+  ;Ad esempio, se vuoi sommare gli elementi di una lista, la tua funzione binaria sarà una funzione che somma due numeri.
+
+L'accumulatore iniziale, che è il valore iniziale dell'accumulatore prima di iniziare ad attraversare la lista. Questo è spesso l'elemento neutro per l'operazione che si desidera eseguire. Ad esempio, per la somma, l'accumulatore iniziale sarà 0.
+
+La lista di elementi su cui desideri eseguire l'operazione.
   (foldl merge '() data))
 
 
