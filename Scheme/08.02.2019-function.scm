cbfecab0f6fe6ac7941de6cf8aad54a36e892d18
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-26 17:15:28 +0200

    Update 08.02.2019-function.scm

--- Scheme/08.02.2019-function.scm
+++ Scheme/08.02.2019-function.scm
@@ -20,13 +20,6 @@
 
 (define (multi-merge . data)
   ;apply merge to data and store result of foldl in a new list
-  ;why foldl? Tried in Dr Racket with foldr->same result; but still: why does it work with foldl?
-  ;how is it possible that merge takes two inputs but I pass an arbitrary number of them and it still works?
-  ;are the two issues related? 
-  ;Input: '(1 2 3 4 8) '(-1 5 6 7) '(0 3 8) '(9 10 12)
-  ;1.merge  '(0 3 8) '(9 10 12) = (0 3 8 9 10 12) -> salvo in '()
-  ;2. merge   '(1 2 3 4 8) '(-1 5 6 7) = (-1 1 2 3 4 5 6 7 8)->nel '() iniziale, ora ho ((0 3 8 9 10 12), (-1 1 2 3 4 5 6 7 8))
-  ;3. merge  ((0 3 8 9 10 12), (-1 1 2 3 4 5 6 7 8))= (-1 0 1 2 3 3 4 5 6 7 8 8 9 10 12)
   
   (foldl merge '() data))
 
