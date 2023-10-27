f1509146a8f223955b4c04b7721a883fd0ffb916
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-10-27 16:41:19 +0200

    Update 01.09.2017-function.scm

--- Scheme/01.09.2017-function.scm
+++ Scheme/01.09.2017-function.scm
@@ -12,6 +12,41 @@
 ;(check-this '(6 h a b a 1 h h i b z 2)) is #f (wrong structure)
 
 
+;CORRECTED SOL:
+
+(define (check-this L)
+  
+   ;definre variabili per valori di ritorno intero della helper function ricorsiva che controlla valore positivo o negativo-> se negativo ritorna falso senza sommare
+   ;es. 6 h a a 1 2 2 b
+   ;input: #ab , #12, res, L
+   (define (helper countab count12 res L)
+     
+     (if (or (< countab 0)(< count12 0))
+         #f
+ 
+     ;else
+         (if (null? L)
+             res
+
+             ;else
+             (let ((head (car L)) (rest (cdr L)))
+ 
+               (cond
+                 ((eq? head 'a) ( (helper (+ countab 1) count12 res rest) ))
+                 ((eq? head 'b) ( (helper (- countab 1) count12 (+ res 1) rest) ))
+                 ((eq? head 1) ( (helper countab (+ count12 1) res rest) ))
+                 ((eq? head 2) ( (helper countab (- count12 1) (+ res 1) rest) ))
+                 )
+               )
+         )
+
+     )
+     )
+  (helper 0 0 0 L)
+  )
+
+
+
 ;THIS CODE DOESN'T WORK: should find a way to do it without cdr and car since we end up passing void lists to such structure which yields an error->(cdr '()) stop 1 iter 
 
 ; Define a tail-recursive helper function
@@ -63,7 +98,6 @@
 (display (check-this '(6 h a b a 1 h h i 2 b z 1))) ; Output: 3 KO
 (display (check-this '(6 h a b a 1 h h i b z 2) )) ; Output: #f (wrong structure) KO
              
-  
-  
+    
 
 
