323e966a64c2161ae7388a6ac07dbcc9b8fc3ff0
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-04 09:35:46 +0100

    Update Key_Concepts.hs

--- Haskell/Key_Concepts.hs
+++ Haskell/Key_Concepts.hs
@@ -1,1 +1,123 @@
+>Haskell is pure:
+
+
+
+
+
+
+>Call-by-need:
+
+
+
+
+
+
+>Currying:
+
+
+
+
+
+
+
+>Types:
+-User defined
+-Recursive:Lists
+
+
+
+
+>Functions:
+-Definition
+-Polymorphism
+-Composition
+
+
+
+
+>Infinite Lists:
+
+
+
+
+
+>Patterns:
+-Case
+-If
+-let and where
+
+
+
+
+
+>ADT:
+
+
+
+
+
+>Type classes:
+-Eq
+-Ord
+-Num
+-Show
+>Deriving
+
+
+
+
+
+
+>Defining Instances:
+
+
+>Foldable: foldl, foldr
+>Functor: Trees
+>Applicative: Lists, Trees
+-Applicative Functors: Maybe
+>Monads: State, Trees, Lists
+
+
+
+
+
+>I/O:
+>Exceptions:
+
+
+
+
+
+
+
+
+
+>Classical Data Structures: 
+-arrays
+-hash-tables
+-maps
+
+
+
+
+
+
+
+
+
+
+
+
+>
+
+
+
+
+
+
+
+
+
+
+
 
