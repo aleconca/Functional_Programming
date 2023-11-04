321616deafcb5ba3b0c470005dbc792958c8a0ce
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-04 10:58:30 +0100

    Update Key_Concepts.hs

--- Haskell/Key_Concepts.hs
+++ Haskell/Key_Concepts.hs
@@ -1,4 +1,5 @@
 >Haskell is pure:
+Haskell computations are purely functional, meaning that computations do not produce side-effects.
 
 
 
@@ -6,13 +7,59 @@
 
 
 >Call-by-need:
+In order to understand call-by-need we need to understand call-by-name first. In call-by-name, arguments in functions are evaluated in an outermost fashion, 
+this means that functions are applied before their arguments are computed. This is called passing arguments 'by name'. In call-by-name when an arguments is not
+used, it is never evaluated; if it is used several times, it is re-evaluated each time. 
+Call-by-need is a memoized version of call-by-name, meaning that if an argument is needed several times, it is evaluated only once and stored for subsequent compuations.
+
+In Haskell we have an explicit implementation of call-by-need, which is incorporated in the 'Delay' keyword. Delay promises to execute a compuation, moreover it caches 
+the evaluation and stores it for subsequent computations. To force evaluation we 'Force' the evaluation.
 
 
 
 
 
 
->Currying:
+>Currying: (also known as 'Shönfinkelisation' :) )
+In Haskell functions have only one argument. Functions with more than one argument are curried. To better clarify this concept we consider this code in Scheme:
+
+(define (sum-square x)
+    (lambda(y) (+ (* x x) (* y y)))))
+
+The signature becomes C->(C->C), where C is the Complex numbers' field (the one mainly used in Scheme), right associative. In Haskell every function is automatically curried.
+(History corner: see logician Haskell Curry)
+
+We say here that the lambdas in Haskell become: 
+from
+(lambda(x y)(+ 1 x y))
+to
+\x y -> 1+x+y
+
+
+
+
+
+
+>Functions:
+
+-Definition: Functions are defined through a series of equations;
+ex.
+--inc n = n + 1
+length :: [ Integer ] -> Integer
+length [] = 0
+length ( x : xs ) = 1 + length xs
+
+this is also an example of pattern matching: arguments are matched with the right parts of equations, top to bottom; if the match succeeds, the function body is called.
+
+-Polymorphism:
+the previous definition of 'length' could work with any kind of lists, not just those made of integers; indeed, if we omit its type declaration, it is inferred 
+by Haskell as having type
+
+length :: [ a ] -> Integer
+
+lower case letters are type variables, so [a] stands for a list of elements of type a, for any a.
+
+-Composition: '$'
 
 
 
@@ -21,21 +68,23 @@
 
 
 >Types:
--User defined
+
+-Static typing: we need to know the type of everything at compile time; '::' means 'has type'.
+
+-User defined: 
 -Recursive:Lists
 
 
 
 
->Functions:
--Definition
--Polymorphism
--Composition
+
 
 
 
 
 >Infinite Lists:
+
+
 
 
 
@@ -50,7 +99,14 @@
 
 
 
+
+
+
 >ADT:
+
+
+
+
 
 
 
@@ -68,7 +124,15 @@
 
 
 
+
+
 >Defining Instances:
+
+
+
+
+
+
 
 
 >Foldable: foldl, foldr
@@ -81,8 +145,12 @@
 
 
 
+
+
+
 >I/O:
 >Exceptions:
+
 
 
 
@@ -108,7 +176,7 @@
 
 
 
->
+
 
 
 
