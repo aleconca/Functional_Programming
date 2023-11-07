656fa41323b8e8a9f2eaa52e6293d83dc7b12c22
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-07 14:55:48 +0100

    Update 10.02.2022-Question.scm

--- Scheme/10.02.2022-Question.scm
+++ Scheme/10.02.2022-Question.scm
@@ -15,19 +15,28 @@
  
 ;1. What can we use r for? Describe how it works and give some useful examples of its usage.
 
-Define function r with a variable number of inputs.
-If s is a cons node then set s to the first element of the cons node, otherwise set it to 1.
-then define an anonymous procedure that if x<y z=x, sets x=x+s and returns z, otherwise returns y.
-The lambda has the purpose of creating a closure.
-
-(r 1 2 3)
-;1
+;The lambda allows us to save the context sorrounding it; thus: s,x,y.
+;Each new call uses the saved local parameters from the previous call.
+;Updates end once x>y.
+;It is a generator implemented as a closure, which returns the numbers from x to y with step s (+1 if s is not defined). When y
+;is reached, it returns it indefinitely.
 
 
-;2. It makes sense to create a version of r without the y parameter? If the answer is yes, implement
+;2. It makes sense to create a version of r without the y parameter? If the answer is so, implement
 ;such version; if no, explain why.
 
-I would say so, since y is a max value just drop it and return elements
+;Yes, we can eliminate y.
+
+(define (r x y . s)
+ (set! s (if (cons? s) 
+             (car s) 
+             1))
+ (lambda ()
+     (let ((z x))
+         (set! x (+ s x))
+            z)
+         )
+ )
 
 (define (r1 x . s)
  (set! s (if (cons? s) (car s) 1))
