689c3c2018d1284f961e19a48231f70fdc60aabf
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-07 15:26:23 +0100

    Update 16.06.2022-function-lambda.scm

--- Scheme/16.06.2022-function-lambda.scm
+++ Scheme/16.06.2022-function-lambda.scm
@@ -4,17 +4,10 @@
 ;returns their composition.
 ;E.g. (list-to-compose (list f g h)) is the function f(g(h(x)).
 
-;define function
-(define (list-to-compose f . rest)
-  
-  (if (null? rest)
-      ; If there are no more functions in the list, return a lambda function
-      (lambda (x) (f x))
-      
-      ; Else, recursively compose the functions
-      (list-to-compose (compose f (car rest)) (cdr rest)))
-  
-  )
+(define (list-to-compose lst)
+   (lambda (x)
+     (foldr (lambda (y acc)
+   (y acc)) x lst)))
 
 
  
