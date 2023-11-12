0fb472c7e8b786b6e520ef70db07fc5249de9a8b
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-12 15:13:21 +0100

    Create 21.01.2022.hs

---
+++ Haskell/21.01.2022.hs
@@ -0,0 +1,5 @@
+{-
+Consider a Tvtl (two-values/two-lists) data structure, which can store either two values of a given type, or
+two lists of the same type.
+Define the Tvtl data structure, and make it an instance of Functor, Foldable, and Applicative
+-}
