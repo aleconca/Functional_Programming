22e1c7d4b352fd30c2fd6bc1cc44f58a67a312af
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-12 15:10:44 +0100

    Create 01.09.2022.hs

---
+++ Haskell/01.09.2022.hs
@@ -0,0 +1,6 @@
+{-
+We want to implement a binary tree where in each node is stored data, together with the number of nodes
+contained in the subtree of which the current node is root.
+1. Define the data structure.
+2. Make it an instance of Functor, Foldable, and Applicative.
+-}
