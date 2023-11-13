830782e6b3238749b1b0d1d00b65b8c961700ec9
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-13 08:49:56 +0100

    Update 01.09.2022-macro.scm

--- Scheme/01.09.2022-macro.scm
+++ Scheme/01.09.2022-macro.scm
@@ -5,3 +5,25 @@
 ;continuation, we can use the associated construct run-cc (which may take parameters). The composition
 ;of store-cc must be managed using in the standard last-in-first-out approach.
 
+(define saved-cont '())
+
+(define (run-cc . param)
+  (let (c (car saved-cont))
+    (set! saved-cont (cdr saved-cont))
+    (apply c param)
+    )
+  )
+
+(define-syntax store-cc
+  (syntax-rules()
+    ((_ condition body ...)
+     (when condition
+       (call/cc (lambda(x) (set! saved-cc (cons (list x)(saved-cont))) ))
+       (begin
+         body...))))
+  )
+
+;cosa significa fare apply tra una continuation e un parametro?
+;perchè posso definire semplicemente (_ e ...) senza una condizione da verificare? Quell' 'e' corrisponde ad un body?
+;cosa posso/devo mettere nel (syntax-rules()( ))?
+;perchè in questo caso posso salvare direttamente il cons senza convertire in lista?
