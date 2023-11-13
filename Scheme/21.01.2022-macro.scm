6d7c56ab84024857258f7cd0dd6f681c10b5a76f
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-13 09:58:55 +0100

    Update 21.01.2022-macro.scm

--- Scheme/21.01.2022-macro.scm
+++ Scheme/21.01.2022-macro.scm
@@ -18,3 +18,26 @@
 ;9
 ;10
 ;6
+
+(define-syntax block
+  (syntax-rules ( then where <-)
+    ((_ (exp1 ...) then (exp2 ...) where (var <- val1 val2) ...)
+     (begin
+       (let ((var val1) ...)
+         exp1 ...)
+       (let ((var val2) ...)
+         exp2 ...)
+       )
+     )
+    )
+  )
+
+
+(block
+ ((displayln (+ x y))
+  (displayln (* x y))
+  (displayln (* z z)))
+ then
+ ((displayln (+ x y))
+  (displayln (* z x)))
+ where (x <- 12 3)(y <- 8 7)(z <- 3 2))
