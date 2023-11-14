f2f23438f0b5f74d6c0bc99765a4d6e93618b392
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-14 13:28:50 +0100

    Update Key_Concepts.hs

--- Haskell/Key_Concepts.hs
+++ Haskell/Key_Concepts.hs
@@ -1,3 +1,5 @@
+Category theory for programmers: https://unglueit-files.s3.amazonaws.com/ebf/e90890f0a6ea420c9825657d6f3a851d.pdf
+
 >Haskell is pure:
 Haskell computations are purely functional, meaning that computations do not produce side-effects.
 
