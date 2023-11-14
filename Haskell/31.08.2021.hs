8e009e9ae8aa940777963245d00285ceaf5abfe6
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-14 11:35:48 +0100

    Update 31.08.2021.hs

--- Haskell/31.08.2021.hs
+++ Haskell/31.08.2021.hs
@@ -15,13 +15,25 @@ instance Functor Slist where
 1. pure should take a value and put it in some sort of default (or pure) minimal context that still yields that value.
 
 2. <*> takes a functor that has a function in it and another functor and sort of extracts that function from the first functor and then maps 
-it over the second one. When I say extract, I actually sort of mean run and then extract, maybe even sequence. 
+it over the second one.
+
+example:
+class (Functor f) => Applicative f where  
+    pure :: a -> f a  
+    (<*>) :: f (a -> b) -> f a -> f b
+    
+t starts the definition of the Applicative class and it also introduces a class constraint. It says that if we want to make a type constructor 
+part of the Applicative typeclass, it has to be in Functor first. That's why if we know that if a type constructor is part of the Applicative typeclass, 
+it's also in Functor, so we can use fmap on it. 
+
+instance Applicative Maybe where  
+    pure x = Just x
+    Nothing <*> _ = Nothing  
+    (Just f) <*> something = fmap f something
 -}
 instance Applicative Slist where
-    --pure :: a -> f a
     pure v = Slist l pure v
-    --(<*>) :: f (a -> b) -> f a -> f b
-    (Slist x fs)<*>(Slist y xs) = Slist (x*y) (fs<*>xs)
+    (Slist x fs)<*>(Slist y xs) = Slist (x*y) (fs<*>xs) --iterate
 
 makeSlist v = Slist (length v) v
 
