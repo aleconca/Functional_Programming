32fae2bed9dab2a29df2e44a35b5a8383e455f40
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-17 11:00:01 +0100

    Update 21.01.2022.hs

--- Haskell/21.01.2022.hs
+++ Haskell/21.01.2022.hs
@@ -3,3 +3,30 @@ Consider a Tvtl (two-values/two-lists) data structure, which can store either tw
 two lists of the same type.
 Define the Tvtl data structure, and make it an instance of Functor, Foldable, and Applicative
 -}
+
+
+data Tvtl a = Tv a a | Tl [a] [a]
+
+instance Functor Tvtl where
+    fmap f (Tv x y)  = Tv (f x) (f y)
+    fmap f (Tl xs ys) = Tl (fmap f xs) (fmap f ys)
+    
+    
+instance Foldable Tvtl where
+    foldr f z (Tv x y) = f x (f y z)
+    foldr f z (Tl xs ys) = foldr f (foldr f z ys) xs
+    
+    
+(Tv x y) +++ (Tv z w) = Tl [x,y] [y,w]
+(Tv x y) +++ (Tl l r) = Tl (x:l) (y:r)
+(Tl l r) +++ (Tv x y) = Tl (l++[x]) (r++[y])
+(Tl l r) +++ (Tl x y) = Tl (l++x) (r++y)
+
+
+tvtlconcat t = foldr (+++) (Tl [][]) t
+tvtlcmap f t = tvtlconcat $ fmap f t
+
+
+instance Applicative Tvtl where
+    pure x = Tl [x] []
+    x <*> y = tvtlcmap (\f -> fmap f y) x
