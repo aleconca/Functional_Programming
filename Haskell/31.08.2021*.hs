b68f440179ad19445a0599c6838f012b4597a9c6
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-17 10:57:21 +0100

    Update 31.08.2021*.hs

--- Haskell/31.08.2021*.hs
+++ Haskell/31.08.2021*.hs
@@ -34,7 +34,7 @@ class (Functor f) => Applicative f where
     pure :: a -> f a  
     (<*>) :: f (a -> b) -> f a -> f b
     
-t starts the definition of the Applicative class and it also introduces a class constraint. It says that if we want to make a type constructor 
+it starts the definition of the Applicative class and it also introduces a class constraint. It says that if we want to make a type constructor 
 part of the Applicative typeclass, it has to be in Functor first. That's why if we know that if a type constructor is part of the Applicative typeclass, 
 it's also in Functor, so we can use fmap on it. 
 
