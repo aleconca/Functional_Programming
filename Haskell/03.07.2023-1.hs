a9707f0eb247ccd4e966d10ce4c93b7493a6a89c
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-22 11:51:52 +0100

    Update 03.07.2023-1.hs

--- Haskell/03.07.2023-1.hs
+++ Haskell/03.07.2023-1.hs
@@ -6,3 +6,49 @@ in the same order.
 
 3. Make D2L an instance of Functor, Foldable, Applicative.
 -}
+
+{- Grammar:
+    S->Y
+    Y->[a]Y
+    Y-> aY
+    Y->Nil
+    
+    le parentesi esterne le metto io
+    
+    D2Cons2 [2, D2Cons2 [3, 4], 5, D2Cons2 [6]]
+-}
+
+
+data D2L a = D2Nil | D2Cons1 a (D2L a) | D2Cons2 [a] --non puoi annestare con costruttori, ma solo con D2L a
+
+flatten :: D2L a -> [a]
+flatten D2Nil = []
+flatten (D2cons1 x xs) = x : flatten xs
+flatten (D2Cons2 xs ys) = xs ++ flatten ys --xs = lista di elementi 'semplici'; ys=lista di D2L a
+
+
+instance Functor D2L where
+    fmap f D2Nil = D2Nil
+    fmap f (D2Cons1 x xs) = D2Cons1 (f x) (fmap f xs)
+    fmap f (D2Cons2 xs ys) = D2Cons2 (fmap f xs) (fmap f ys)
+    
+    
+
+instance Foldable D2L where
+    foldr f z D2Nil = D2Nil
+    foldr f z (D2Cons1 x xs) = f x (foldr f z xs)
+    --foldr f z (D2Cons2 xs ys) = (foldr f z ( xs ++ flatten ys) )
+    foldr f i (D2Cons2 xs ys) = (foldr f (foldr f i ys) xs)
+
+
+instance Applicative D2L where
+    pure
+    <*>
+
+
+
+
+
+
+
+
