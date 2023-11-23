9aa547cdee1062a286fdd8d3071120a9385ec461
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-23 11:41:47 +0100

    Update 08.02.2021-function.scm

--- Scheme/08.02.2021-function.scm
+++ Scheme/08.02.2021-function.scm
@@ -5,3 +5,32 @@
 ;E.g. (depth-encode '(1 (2 3) 4 (((5) 6 (7)) 8) 9 (((10))))) 
 ;returns
 ;((0 . 1) (1 . 2) (1 . 3) (0 . 4) (3 . 5) (2 . 6) (3 . 7) (1 . 8) (0 . 9) (3 . 10))
+
+
+(define result '())
+(define i -1)
+
+(define (f elem)
+  (let ((v (cons i elem)))
+    (set! result (cons v result ))
+    ;(display v)
+    (display result)
+    (newline)
+  )
+  )
+ 
+
+(define (depth-encode L)
+  (cond
+    ;((null? L) (display result) )
+    ((not (list? L)) (f L))
+    ((null? (filter list? L))  (set! i (+ i 1)) (map f L) (set! i (- i 1)) ) 
+    (else (set! i (+ i 1)) (map depth-encode L) (set! i (- i 1)) )
+  )
+  (foldl cons '() result)
+  )
+
+
+
+(depth-encode '(1 (2 3) 4 (((5) 6 (7)) 8) 9 (((10))))) 
+;'((0 . 1) (1 . 2) (1 . 3) (0 . 4) (3 . 5) (2 . 6) (3 . 7) (1 . 8) (0 . 9) (3 . 10))
