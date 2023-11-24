7036d46f23830630ac3a851a5ed37ec712ca2b93
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-24 10:10:27 +0100

    Update 22.06.2021-Classes-BTTrees.hs

--- Haskell/22.06.2021-Classes-BTTrees.hs
+++ Haskell/22.06.2021-Classes-BTTrees.hs
@@ -1,7 +1,9 @@
 {-
 Define a data-type called BTT which implements trees that can be binary or ternary, and where every
 node contains a value, but the empty tree (Nil). Note: there must not be unary nodes, like leaves.
+
 1) Make BTT an instance of Functor and Foldable.
+
 2) Define a concatenation for BTT, with the following constraints:
 • If one of the operands is a binary node, such node must become ternary, and the other operand
 will become the added subtree (e.g. if the binary node is the left operand, the rightmost node of
