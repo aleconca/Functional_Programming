d4cf228abc7cfc88fe42404f5e675824304159b1
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-25 09:57:05 +0100

    Update 07.02.2020-macro.scm

--- Scheme/07.02.2020-macro.scm
+++ Scheme/07.02.2020-macro.scm
@@ -11,3 +11,28 @@
   ;(display " "))
 
 ;shows on the screen: 3 6 9
+
+
+(define-syntax each-until
+  (syntax-rules(in until :)
+    ((_ var in L until condition : body ...)
+     (let loop ((xs L))   
+       (let ((var (car xs)))
+         ;(display var)
+         ;(display xs)
+         (when (not condition)
+           (begin
+             body ...)
+           (loop (cdr xs) ))
+                                
+         )
+       )
+     )))
+  
+
+
+(each-until x in '(1 2 3 4)
+  until (> x 3) :
+  (display (* x 3))
+  (display " ")
+  )
