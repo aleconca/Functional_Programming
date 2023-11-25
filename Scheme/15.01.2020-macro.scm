71fb0bb44644f981f5e0f2b15732b90f6ddb2909
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-25 09:58:35 +0100

    Create 15.01.2020-macro.scm

---
+++ Scheme/15.01.2020-macro.scm
@@ -0,0 +1,6 @@
+#lang racket
+
+;Consider the Foldable and Applicative type classes in Haskell. We want to implement something
+;analogous in Scheme for vectors. Note: you can use the following library functions in your code: vectormap, vector-append.
+;1) Define vector-foldl and vector-foldr.
+;2) Define vector-pure and vector-<*>.
