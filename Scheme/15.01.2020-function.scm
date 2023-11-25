220ae619362e6cb7b6a14ed4bf80597d118a4c78
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-25 10:42:09 +0100

    Update 15.01.2020-function.scm

--- Scheme/15.01.2020-function.scm
+++ Scheme/15.01.2020-function.scm
@@ -4,3 +4,21 @@
 ;analogous in Scheme for vectors. Note: you can use the following library functions in your code: vectormap, vector-append.
 ;1) Define vector-foldl and vector-foldr.
 ;2) Define vector-pure and vector-<*>.
+
+
+(define (vector-foldl vect)
+  (let ((result (make-vector 0 vect)))
+               (vector-map (lambda(x)(set! result (vector-append (make-vector 1 x) result)) ) vect)
+               result)
+  )
+(vector-foldl #(1 2 3))
+
+(define (vector-foldr vect)
+  (let ((result (make-vector 0 vect)))
+               (vector-map (lambda(x)(set! result (vector-append (make-vector 1 x) result)) ) (vector-foldl vect))
+               result)
+  )
+(vector-foldr #(1 2 3))
+
+
+
