8dd5d335f210479058cbe3816bf0db65acf9439d
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-11-29 11:33:16 +0100

    Create 24.07.2019.scm

---
+++ Scheme/24.07.2019.scm
@@ -0,0 +1,6 @@
+#lang racket
+
+;Write a functional, tail recursive implementation of a procedure that takes a list of numbers L and two values
+;x and y, and returns three lists: one containing all the elements that are less than both x and y, the second one
+;containing all the elements in the range [x,y], the third one with all the elements bigger than both x and y. It
+;is not possible to use the named let construct in the implementation.
