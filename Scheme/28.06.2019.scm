4b7c6552d9d2e82abc949ea4eba38755fbd01e51
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-01 11:36:42 +0100

    Update 28.06.2019.scm

--- Scheme/28.06.2019.scm
+++ Scheme/28.06.2019.scm
@@ -7,3 +7,54 @@
 ;(define t1 (Branch (Branch (Leaf 1) -1 (Leaf 2)) -2 (Leaf 3)))
 ;((t1 'map (lambda (x) (+ x 1))) 'print)
 ;should display: (Branch (Branch (Leaf 2) 0 (Leaf 3)) -1 (Leaf 4))
+
+
+
+(define (Branch t1 x t2)
+  
+   (define (print)
+     (display "(Branch ")
+     (t1 'print)
+     (display " ")
+     (display x)
+     (display " ")
+     (t2 'print)
+     (display ")")
+     )
+
+  (define (map f)
+    (Branch (t1 'map f) (f x) (t2 'map f)))
+
+   (lambda (message . args)
+     (apply (case message
+              ((map) map)
+              ((print) print)
+              (else (error "Unknown method")))
+            args))
+   )
+  
+(define (Leaf x)
+  
+   (define (print)
+     (display "(Leaf ")
+     (display " ")
+     (display x)
+     (display " ")
+     )
+
+  (define (map f)
+    (Leaf (f x)) )
+
+   (lambda (message . args)
+     (apply (case message
+              ((map) map)
+              ((print) print)
+              (else (error "Unknown method")))
+            args))
+   )
+  
+
+
+(define t1 (Branch (Branch (Leaf 1) -1 (Leaf 2)) -2 (Leaf 3)))
+((t1 'map (lambda (x) (+ x 1))) 'print)
+;should display: (Branch (Branch (Leaf 2) 0 (Leaf 3)) -1 (Leaf 4))
