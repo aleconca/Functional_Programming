71c502465f3a01907a011305ca0fa125aae83f0d
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-04 11:36:03 +0100

    Update 25.01.2023-Classes-fun-TM.hs

--- Haskell/25.01.2023-Classes-fun-TM.hs
+++ Haskell/25.01.2023-Classes-fun-TM.hs
@@ -13,3 +13,52 @@ values are the same and in the same order, regardless of the position of their h
 3. Define the two functions left and right, to move the position of the head on the left and on the right.
 4. Make Tape an instance of Functor and Applicative.
 -}
+
+
+--1)
+data Tape a = Tape [a] a [a] a
+
+--data Tape a b = Tape a (Tape a b) (Tape a b) | Blank b (Tape a b)
+
+
+--2)
+instance Eq a => Eq (Tape a) where
+ (Tape x h y _) == (Tape x' h' y' _) = (x ++ [h] ++ y) == (x' ++ [h'] ++ y')
+
+    
+    
+instance Show a => Show (Tape a) where
+    Tape l h r b = show l ++ show h ++ show r 
+ 
+
+--3)
+
+left :: Tape a -> Tape a
+left (Tape [] h r b) = Tape [] b ([h]++r) b
+left (Tape (x:xs) h r b) = Tape xs x ([h]++r) b --Shouldn't I reverse the left part?
+
+    
+right :: Tape a -> Tape a  
+right (Tape x h [] b) = Tape ([h]++l) b [] b
+right (Tape l h (x:xs) b) = Tape ([h]++l) x xs b
+
+
+--4) 
+--fmap :: (a -> b) -> f a -> f b 
+instance Functor Tape where
+    fmap f (Tape l h r b) = Tape (fmap f l) (f h) (fmap f r) (f b)
+
+
+(Tape [] b [] b)+++(Tape l h r b) = Tape l++[] h []++r b
+
+concatTape t = foldr (+++) (Tape [] b [] b) t
+concatMapTape f t = concatTape(fmap f t)
+
+--pure :: a -> f a  
+--(<*>) :: f (a -> b) -> f a -> f b
+instance Applicative Tape where
+    pure x = Tape [] x [] x
+ -- zipwise apply
+    (Tape fx fc fy fb) <*> (Tape x c y b) = Tape (zipApp fx x) (fc c) (zipApp fy y) (fb b)
+                                            where zipApp x y = [f x | (f,x) <- zip x y] --list comprehension
+                                            -- fx=(f1,f2,f3) e x=Char allora [(f1,x),(f3,x),(f3,x)] ed applico f x se ho un pair (f,x) e return list
