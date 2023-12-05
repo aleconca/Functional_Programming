f93c70c99abf7693088ffb5be25f5c40ade7a1d4
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-05 10:21:18 +0100

    Update and rename 16.01.2019-function.scmm to 16.01.2019-function.scm

--- Scheme/16.01.2019-function.scm
+++ Scheme/16.01.2019-function.scm
@@ -2,3 +2,11 @@
 
 ;Define a pure function f with a variable number of arguments, that, when called like (f x1 x2 .. xn), returns:
 ;(xn (xn-1 ( .. (x1 (xn xn-1 .. x1))..). Function f must be defined using only fold operations for loops.
+
+
+(define (f . args)
+  (foldr (lambda(x y)(cons x (list y) )) (foldl cons '() args) args)
+  )
+
+(f 'x1 'x2 'x3 'x4)
+;'(x1 (x2 (x3 (x4 (x4 x3 x2 x1)))))
