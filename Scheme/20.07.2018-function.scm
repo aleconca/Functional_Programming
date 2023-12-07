9d4675a31002f830998e9097853f57a4f15411b7
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-07 09:53:13 +0100

    Create 20.07.2018-function.scm

---
+++ Scheme/20.07.2018-function.scm
@@ -0,0 +1,4 @@
+#lang racket
+
+;1) Give a purely functional definition of fep, which takes a list (x1 x2 ... xn) and returns (x1 (x2 (... (xn (x1 x2 ... xn) xn) xn-1) ...) x1).
+;2) Consider the following code; explain how it works, and what is the output of the call (run).
