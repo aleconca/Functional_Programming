cedbcbda88628f15c6742b1c5143debd4263ddb5
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-09 10:43:07 +0100

    Update Key_concepts.erl

--- Erlang/Key_concepts.erl
+++ Erlang/Key_concepts.erl
@@ -411,4 +411,29 @@ Any process can send a message to a registered process.
 
 
 
+>Client-Server Model:
+
+-module(myserver).
+
+server(Data) -> % note: local data
+  receive
+    {From,{request,X}} ->
+    {R, Data1} = fn(X, Data),
+    From ! {myserver,{reply, R}},
+    server(Data1)
+  end.
+
+-export([request/1]).
+
+request(Req) ->
+  myserver ! {self(),{request,Req}},
+    receive
+    {myserver,{reply,Rep}} -> Rep
+  end.
+
+
+
+
+
+
 
