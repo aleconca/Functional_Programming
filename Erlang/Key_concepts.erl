d4f08891517bc21809f469565356a27febd8ac18
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-09 09:29:32 +0100

    Update Key_concepts.erl

--- Erlang/Key_concepts.erl
+++ Erlang/Key_concepts.erl
@@ -1,4 +1,34 @@
 https://learnyousomeerlang.com/content
 https://www.erlang.org/
 
+>Variables:
+
+>Atoms:
+
+>Tuples:
+
+>Lists:
+
+>Pattern Matching:
+
+>Maps:
+
+>Function Calls:
+>BIFs:
+>Function Syntax and Evaluation:
+>Guarded Functions:
+
+>Apply
+
+>Lambdas
+
+>hof
+
+>Concurrency
+
+
+
+
+
+
 
