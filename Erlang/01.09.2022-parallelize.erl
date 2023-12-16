66a2b7e05ee7f300e825f5f7b7ddf98199f78247
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-16 12:42:14 +0100

    Update 01.09.2022-parallelize.erl

--- Erlang/01.09.2022-parallelize.erl
+++ Erlang/01.09.2022-parallelize.erl
@@ -2,9 +2,28 @@
 %is the number of parallel processes in which to split the evaluation of the fold. Being F associative,
 %parfold can evaluate foldl on the N partitions of L in parallel. Notice that there is no starting (or
 %accumulating) value, differently from the standard foldl.
-%You may use the following libray functions:
 
+%You may use the following libray functions:
 %lists:foldl(<function>, <starting value>, <list>)
 %lists:sublist(<list>, <init>, <length>), which returns the sublist of <list> starting at position
-
 %<init> and of length <length>, where the first position of a list is 1.
+
+partition(L,Init,N,Acc) when length(L) > length(L) div N ->
+    Acc=Acc++[lists:sublist(L, Init, length(L) div N],
+    partition(L, Init+(length(L) div N), N, Acc).
+partition(L,Init,N,Acc) -> Acc++[lists:sublist(L, Init, length(L)-Init)].    
+
+job(Parent, F, [X|Xs]) ->
+    Self=self(),
+    V=lists:foldl(F,X,Xs),
+    Parent ! {result,Self,V}
+
+parfold(F, L, N) -> 
+    Result = partition(L,1,N,[]),
+    Self=self(),
+    Plist = [spawn(?MODULE, job, [Self,F,X]) || X <- Result],
+    [X|Xs] = [receive
+                {result,P,V} -> V
+              end || P <- Plist ],
+    list:foldl(F, X, Xs).
+    
