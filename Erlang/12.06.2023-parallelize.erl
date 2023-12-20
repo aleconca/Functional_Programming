e26e730a3f4d10d7ead28f6b8854eaf3ce6c5d38
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-20 10:59:45 +0100

    Update and rename 12.06.2023.erl to 12.06.2023-parallelize.erl

--- Erlang/12.06.2023-parallelize.erl
+++ Erlang/12.06.2023-parallelize.erl
@@ -9,3 +9,26 @@
 %merge(L1, [], A) -> A ++ L1;
 %merge([H1|T1], [H2|T2], A) when H2 >= H1 -> merge(T1, [H2|T2], A ++ [H1]);
 %merge([H1|T1], [H2|T2], A) when H1 > H2 -> merge([H1|T1], T2, A ++ [H2]).
+
+
+mergesortp(L) ->
+ Me = self(),
+ msp(Me, L),
+ receive
+    {Me, R} -> R
+ end.
+ 
+msp(Pid, [L]) -> Pid ! {self(), [L]};
+msp(Pid, L) ->
+ {L1, L2} = lists:split(length(L) div 2, L),
+ Me = self(),
+ Pid1 = spawn(fun() -> msp(Me, L1) end),
+ Pid2 = spawn(fun() -> msp(Me, L2) end),
+ receive
+    {Pid1, Sorted1} ->
+    receive
+        {Pid2, Sorted2} -> Pid ! {self(), merge(Sorted1, Sorted2)}
+    end
+ end.
+ 
+ 
