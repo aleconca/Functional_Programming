0a6c8fc3763caa3f74a27a6f07a3f7467762b720
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-21 10:05:36 +0100

    Update 12.09.2023.erl

--- Erlang/12.09.2023.erl
+++ Erlang/12.09.2023.erl
@@ -4,3 +4,18 @@
 %2. Define a function incbtrees to create a process corresponding to the infinite tree of Exercise 2.2.
 %Notes: for security reasons, processes must only answer to their creating process; to define trees, you can use
 %suitable tuples with atoms as customary in Erlang (e.g. {branch, {leaf, 1}, {leaf, 1}}).
+
+
+%1. Define a function btrees which takes a value x and returns an infinite list of binary trees, where:
+
+  %1. all the leaves contain x,
+  %2. each tree is complete,
+  %3. the first tree is a single leaf, and each tree has one level more than its previous one in the list.
+
+%data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+%--1)
+%btrees :: a -> [Tree a]
+%btrees x = helper(Leaf x)  
+           % where helper tree =
+                  %  tree : (helper (Node tree tree) )
