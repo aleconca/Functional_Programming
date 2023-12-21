c79a7b1810d8589f3aba7fa14d4f8dd031a0d7f3
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-21 10:57:34 +0100

    Update 12.09.2023.erl

--- Erlang/12.09.2023.erl
+++ Erlang/12.09.2023.erl
@@ -16,7 +16,21 @@ btrees x = helper(Leaf x)
            where helper tree =
                   tree : (helper (Node tree tree) )
                   
-                  
+
+
+child(P,T) ->
+    receive
+        {next} -> T1 = {branch, T, T},
+                        P ! T1,
+                        child(P,T1);
+        {terminate} -> T
+    end.
+    
+helper(V,P) ->
+    spawn(?MODULE, child, [Self,{leaf V}]).
+
+
+              
 %2. Define a function incbtrees to create a process corresponding to the infinite tree of Exercise 2.2.
 
 %2. Define an infinite list of binary trees, which is like the previous one, but the first leaf contains the integer 1,
@@ -31,6 +45,28 @@ btrees2 :: a -> [Tree a]
 btrees2 x = helper(Leaf x)  
             where helper tree =
                     tree : (helper (fmap (+1) (Node tree tree)) )
+
+
+
+
+inctree({leaf, X}) ->
+    {leaf, X+1};
+inctree({branch, X, Y}) ->
+    {branch, inctree(X), inctree(Y)}.
+ 
+incbtrees_body(T, Pid) ->
+ receive
+     next -> T1 = inctree(T),
+             T2 = {branch, T1, T1},
+             Pid ! T2,
+             incbtrees_body(T2, Pid);
+    stop -> T
+ end.
+ 
+incbtrees(Pid) ->
+ spawn(?MODULE, incbtrees_body, [{leaf, 0}, Pid]).
+
+
 
 %Notes: for security reasons, processes must only answer to their creating process; to define trees, you can use
 %suitable tuples with atoms as customary in Erlang (e.g. {branch, {leaf, 1}, {leaf, 1}}).
