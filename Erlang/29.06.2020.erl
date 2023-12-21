d9ea8712b264fb5033eced34932bae322f774f9b
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-21 09:30:30 +0100

    Create 29.06.2020.erl

---
+++ Erlang/29.06.2020.erl
@@ -0,0 +1,6 @@
+%Define a "functional" process buffer, called fuffer, that stores only one value and may receive messages only from its creator. fuffer can receive the following commands:
+%'set' to store a new value
+%'get' to obtain the current value
+%'apply F' to apply the function F to the stored value
+%'die' to end
+%'duplicate' to create (and return) an exact copy of itself
