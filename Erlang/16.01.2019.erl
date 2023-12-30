b30cb02ee23a734acc62f2db94e10ca5365c1d16
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-30 09:54:39 +0100

    Create 16.01.2019.erl

---
+++ Erlang/16.01.2019.erl
@@ -0,0 +1,6 @@
+%Define a process P, having a local behavior (a function), that answer to three commands:
+%- load is used to load a new function f on P: the previous behavior is composed with f;
+%- run is used to send some data D to P: P returns its behavior applied to D;
+%- stop is used to stop P.
+%For security reasons, the process must only work with messages coming from its creator: other messages
+%must be discarded.
