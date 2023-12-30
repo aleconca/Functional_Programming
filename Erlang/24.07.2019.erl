14d0b33af12a2200867ee30df73da3adf0eeff9e
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-30 10:16:39 +0100

    Update 24.07.2019.erl

--- Erlang/24.07.2019.erl
+++ Erlang/24.07.2019.erl
@@ -4,3 +4,26 @@
 
 %Note: for simplicity, you can use the library function spawn_link/1, which takes a lambda function, and
 %spawns and links a process running it.
+
+listlink([], Pids) -> Pids;
+listlink([F|Fs], Pids) ->
+  Pid = spawn_link(F),
+  listlink(Fs, Pids#{Pid => F}).
+
+master(Functions) ->
+  process_flag(trap_exit, true),
+  Workers = listlink(Functions, #{}),
+  master_loop(Workers, length(Functions)).
+
+master_loop(Workers, Count) ->
+  receive
+    {'EXIT', Child, normal} ->
+    if
+      Count =:= 1 -> ok;
+      true -> master_loop(Workers, Count-1)
+    end;
+  {'EXIT', Child, _} ->
+  #{Child := Fun} = Workers,
+  Pid = spawn_link(Fun),
+  master_loop(Workers#{Pid => Fun}, Count)
+  end.
