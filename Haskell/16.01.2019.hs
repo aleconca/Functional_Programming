416aac48ab40f06c577cace896ff1b16e62078cf
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-31 10:04:17 +0100

    Create 16.01.2019.hs

---
+++ Haskell/16.01.2019.hs
@@ -0,0 +1,7 @@
+{-We want to define a data structure, called Listree, to define structures working both as lists and as binary
+trees, like in the next figure.
+1) Define a datatype for Listree.
+2) Write the example of the figure with the defined data structure.
+3) Make Listree an instance of Functor.
+4) Make Listree an instance of Foldable.
+5) Make Listree an instance of Applicative.-}
