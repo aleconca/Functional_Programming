6297ecda52f6f580e10abff34dfbbbcc00101ae9
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2023-12-31 10:05:19 +0100

    Create 28.06.2019.hs

---
+++ Haskell/28.06.2019.hs
@@ -0,0 +1,5 @@
+{-1) Define a Tritree data structure, i.e. a tree where each node has at most 3 children, and every node contains
+a value.
+2) Make Tritree an instance of Foldable and Functor.
+3) Define a Tritree concatenation t1 +++ t2, where t2 is appended at the bottom-rightmost position of t1.
+4) Make Tritree an instance of Applicative.-}
