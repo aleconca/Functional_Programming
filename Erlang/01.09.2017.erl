b2322cff6a8bb8ad8f32670a9c9542c372e93e13
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2024-01-01 10:30:18 +0100

    Update 01.09.2017.erl

--- Erlang/01.09.2017.erl
+++ Erlang/01.09.2017.erl
@@ -3,3 +3,41 @@
 %returns false.
 
 %E.g. parfind([[1,2,3],[4,5,6],[4,5,9,10]], 4) could return either [4,5,6] or [4,5,9,10]; parfind([[1,2,3],[4,5,6],[4,5,9,10]], 7) is false.
+
+
+lookup(L,X,S) ->
+    [ if 
+         V =:= X -> S ! L ;
+         true -> false
+      end
+    || V <- L]    
+
+parfind(Lists,X) ->
+    Self = self(),
+    [ spawn(?MODULE,lookup,[L,X,Self]) || L <- Lists],
+    receive
+        V -> V;
+        false -> ok
+    end.
+
+%%%
+
+worker(Dad, List, X) ->
+    case lists:member(X, List) of
+        true -> Dad ! {found, List};
+        false -> Dad ! nay
+    end.
+
+get_result(0) -> false;
+get_result(V) ->
+    receive
+        nay -> get_result(V-1);
+        {found, L} -> L
+    end.
+
+parfind(LofL, X) ->
+    lists:foreach(fun(L) -> spawn(?MODULE, worker, [self(), L, X]) end, LofL),
+    get_result(length(LofL)).
+
+%%%
+
