d0c913e496e1e9354045ba022fbfbc4d9ac29d4d
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2024-01-02 10:17:24 +0100

    Update 12.06.2023-parallelize*.erl

--- Erlang/12.06.2023-parallelize*.erl
+++ Erlang/12.06.2023-parallelize*.erl
@@ -6,6 +6,7 @@
 % merge(mergesort(L1), mergesort(L2)).
 
 %merge(L1, L2) -> merge(L1, L2, []).
+
 %merge([], L2, A) -> A ++ L2;
 %merge(L1, [], A) -> A ++ L1;
 %merge([H1|T1], [H2|T2], A) when H2 >= H1 -> merge(T1, [H2|T2], A ++ [H1]);
