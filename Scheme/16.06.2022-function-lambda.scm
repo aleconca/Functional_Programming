0d574466157f024805cfee51c4256597cc8542b3
Alessia Conca Roncari <145048312+aleconca@users.noreply.github.com>
2024-01-05 12:01:55 +0100

    Update 16.06.2022-function-lambda.scm

--- Scheme/16.06.2022-function-lambda.scm
+++ Scheme/16.06.2022-function-lambda.scm
@@ -4,8 +4,6 @@
 ;returns their composition.
 ;E.g. (list-to-compose (list f g h)) is the function f(g(h(x)).
 
-;Define a list-to-compose pure function, which takes a list containing functions of one argument and
-;returns their composition.
 
 ;input: list of functions, their common argument
 (define (list-to-compose L)
@@ -16,16 +14,6 @@
 
 (list-to-compose (list 'f 'g 'h)) ;is the function f(g(h(x))
 
-;1:
-;take current element of L
-;take acc=x (first iteration) lambda does this: (h x)
 
-;2:
-;take current element of L
-;take acc=(f x) lambda does this: (g (h x))
-
-;3:
-;take current element of L
-;take acc=(g(h x)) lambda does this: (f(g (h x)))=x
 
  
